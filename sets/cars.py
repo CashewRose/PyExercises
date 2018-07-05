e0673977f8ce541510be1e0f96256756a9100666
CashewRose <danieagnoletti@gmail.com>
2018-07-05 12:05:29 -0500

    Final Ex. completed

--- sets/cars.py
+++ sets/cars.py
@@ -1,7 +1,15 @@
 showroom = set()
+
 showroom.update({'Toyota Corolla', 'Honda Civic', 'Tesla Model S', 'Maserati GranTurismo'})
 print(len(showroom))
 showroom.add("Toyota Corolla")
 print(showroom)
 showroom.update({'Volkswagen Beetle Convertable', 'Smart ForTwo'})
 showroom.discard('Honda Civic')
+
+junkyard = set(['Toyota Camry', 'Honda Civic', 'Smart ForTwo', 'Tesla Model 3'])
+
+same = showroom.intersection(junkyard)
+both = showroom.union(junkyard)
+withoutOneJunkyardCar = both.discard('Toyota Camry')
+
